b900bfa9e2ec0917be5d3eb6e62ad06882cf578a
Zach Bush <zach@zmbush.com>
2017-11-29 19:50:59 -0800

    Tweaks

--- src/index.js
+++ src/index.js
@@ -2,28 +2,27 @@
 
 const loaders = require('./loaders');
 
-/**
- * Quick workaround allowing GraphQL to access model attributes directly
- * (to access a bookshelf model attribute (like model.name), we have to use the .get() method)
- *
- * @param {object} collection
- * @returns {*}
- */
-function exposeAttributes(collection) {
-    function exposeModelAttributes(item) {
-        // Make sure that relations are excluded
-        return Object.assign(item, item.serialize({ shallow: true }));
-    }
-    if (collection) {
-        if (collection.hasOwnProperty('length')) {
-            return collection.map((item) => { return exposeModelAttributes(item); });
-        }
-        return exposeModelAttributes(collection);
-    }
-    return collection;
-}
-
 module.exports = {
+    /**
+     * Quick workaround allowing GraphQL to access model attributes directly
+     * (to access a bookshelf model attribute (like model.name), we have to use the .get() method)
+     *
+     * @param {object} collection
+     * @returns {*}
+     */
+    exposeAttributes(collection) {
+        function exposeModelAttributes(item) {
+            // Make sure that relations are excluded
+            return Object.assign(item, item.serialize({ shallow: true }));
+        }
+        if (collection) {
+            if (collection.hasOwnProperty('length')) {
+                return collection.map((item) => { return exposeModelAttributes(item); });
+            }
+            return exposeModelAttributes(collection);
+        }
+        return collection;
+    },
 
     /**
      *
@@ -41,7 +40,10 @@ module.exports = {
     resolverFactory(Model) {
         return function resolver(modelInstance, args, context, info, extra) {
             const isAssociation = (typeof Model.prototype[info.fieldName] === 'function');
-            const model = isAssociation ? modelInstance.related(info.fieldName) : new Model();
+            if (!(modelInstance instanceof Model)) {
+              modelInstance = new Model();
+            }
+            const model = isAssociation ? modelInstance.related(info.fieldName) : modelInstance;
             for (const key in args) {
                 model.where(`${model.tableName}.${key}`, args[key]);
             }
